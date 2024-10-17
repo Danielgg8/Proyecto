0823460fdafc64fe6cbe0d332e88c86650a7eb9e
Danielgg8 <danielgranda21@gmail.com>
2024-10-17 18:04:31 +0200

    Comentario
    Comentario

---
+++ Archivo.R
@@ -0,0 +1,14 @@
+install.packages("readr")
+library(dplyr)
+
+
+library(readr)
+
+spotify_data <- read_csv("C:\\Users\\danie\\Desktop\\2do Año\\UNIT 24 - ADVANCED PROGRAMMING FOR DATA ANALYSIS\\Spotify Most Streamed Songs.csv")
+
+
+head(spotify_data)
+View(spotify_data)
+#ESTO ES UN CAMBIO
+
+
