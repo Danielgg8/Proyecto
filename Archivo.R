d986ac9d93b9b2a1c8a3bdb35dca03a70892ad7a
Danielgg8 <danielgranda21@gmail.com>
2024-10-23 18:14:34 +0200

    Limpiar fecha
    Columnas de year, month y day eliminadas y combinadas en una sola llamada released_date

--- Archivo.R
+++ Archivo.R
@@ -1,14 +1,17 @@
 install.packages("readr")
 library(dplyr)
-
-
 library(readr)
 
 spotify_data <- read_csv("C:\\Users\\danie\\Desktop\\2do Año\\UNIT 24 - ADVANCED PROGRAMMING FOR DATA ANALYSIS\\Spotify Most Streamed Songs.csv")
 
+spotify_data$release_date <- as.Date(paste(spotify_data$released_year, spotify_data$released_month, spotify_data$released_day, sep = "-"))
+
+spotify_data$released_year <- NULL
+spotify_data$released_month <- NULL
+spotify_data$released_day <- NULL
 
 head(spotify_data)
 View(spotify_data)
-#ESTO ES UN CAMBIO
+
 
 
